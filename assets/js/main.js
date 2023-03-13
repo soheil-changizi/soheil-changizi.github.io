00e943adea3b1d89e104c35cffe53e29ee3b14a8
soheil-changizi <42040757+soheil-changizi@users.noreply.github.com>
2023-03-13 15:38:14 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -218,31 +218,7 @@
     selector: '.portfolio-lightbox'
   });
 
-  /**
-   * Initiate portfolio details lightbox 
-   */
-  const portfolioDetailsLightbox = GLightbox({
-    selector: '.portfolio-details-lightbox',
-    width: '90%',
-    height: '90vh'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+  
 
   /**
    * Initiate Pure Counter 
