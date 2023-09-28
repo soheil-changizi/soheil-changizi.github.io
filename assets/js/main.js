7e5f242ded2c32e8cba55c3b5b6657188b101bd1
soheil-changizi <42040757+soheil-changizi@users.noreply.github.com>
2023-09-28 13:23:46 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,6 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-
-import Typed from 'typed.js';
-
 (function() {
   "use strict";
 
@@ -221,7 +218,31 @@ import Typed from 'typed.js';
     selector: '.portfolio-lightbox'
   });
 
-  
+  /**
+   * Initiate portfolio details lightbox 
+   */
+  // const portfolioDetailsLightbox = GLightbox({
+  //   selector: '.portfolio-details-lightbox',
+  //   width: '90%',
+  //   height: '90vh'
+  // });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
 
   /**
    * Initiate Pure Counter 
